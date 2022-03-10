4b870f05afdb77f70b56f4b92c9cd4be30b08db0
bishop254 <karanjachiuri@gmail.com>
2022-03-10 09:43:28 +0300

    Created a c file that uses puts for printing text

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Function with our logic for printing.
+ *  puts is used since is is cheaper than printf.
+ * return: 0 
+ */
+
+int main(void) {
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
