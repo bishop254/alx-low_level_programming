cf980459525f37bb01a128e043e3694ee1e454af
bishop254 <karanjachiuri@gmail.com>
2022-03-13 11:15:39 +0300

    Fifth c file editted

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Description: prints lowercase and uppercase alphabets using puts
+ * Return: 0
+ */
+int main(void)
+{
+char alpha;
+for (alpha = 'a'; alpha <= 'z'; ++alpha)
+{
+if (alpha != 'q' || alpha != 'e')
+{
+putchar(alpha);
+}
+}
+putchar('\n');
+return (0);
+}
