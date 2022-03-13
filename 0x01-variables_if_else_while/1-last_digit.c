533c3465159a3913b1fca2e1dc6e2aff71e814a2
bishop254 <karanjachiuri@gmail.com>
2022-03-13 10:38:54 +0300

    Second c file added

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Description: if conditions to check if the last digit of 
+ *              a number is +ve or -ve
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int lastD;
+    
+    lastD = n % 10;
+    if (lastD > 5)
+    {
+        printf("Last digit of %d is %d and is greater than 5\n", n, lastD);
+    } else if (lastD == 0)
+    {
+        printf("Last digit of %d is %d and is 0\n", n, lastD);
+    } else if (lastD < 0)
+    {
+        printf("Last digit of %d is %d and is negative\n", n, lastD);
+
+    }
+	return (0);
+}
