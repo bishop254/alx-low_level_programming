972e063c1e479f81446e49b70aef7642d867a4bb
bishop254 <karanjachiuri@gmail.com>
2022-03-13 11:40:04 +0300

    Added the tenth c file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Description: prints all possible combinations 
+ * for single-digit numbers using putchar
+ * Return: 0
+ */
+int main(void)
+{
+int num;
+
+for (num = 0; num < 10; num++)
+{
+putchar(num + '0');
+if (num < 9)
+{
+putchar(44);
+putchar(32);
+}
+}
+putchar('\n');
+return (0);
+}
