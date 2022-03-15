886c9edcd43322e01dde64987fd1006169fd1017
bishop254 <karanjachiuri@gmail.com>
2022-03-15 22:37:41 +0300

    Editted the seventh script

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 
 /**
  * _abs - main function
- * @int: integer from which we will compute the absolute value
+ * @n: integer from which we will compute the absolute value
  * Description: prints absolute value of an integer
  * Return: absolute value
  */
@@ -11,7 +11,7 @@ int _abs(int n)
 {
 if (n > 0)
 {
-return(n);
+return (n);
 }
 else if (n == 0)
 {
