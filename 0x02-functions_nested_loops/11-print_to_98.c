cf3cd91a620828d17932175d8b6578ab1e2ce810
bishop254 <karanjachiuri@gmail.com>
2022-03-16 00:44:51 +0300

    Added the last c file

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - main function
+ * Description: prints natural numbers from a specific integer to 98
+ * @n: starting point
+ * Return: result of addition
+ */
+
+void print_to_98(int n)
+{
+int count;
+if (n < 99)
+{
+for (count = n; count <= 98; count++)
+{
+printf("%d", count);
+if (count < 98)
+{
+printf(", ");
+}
+}
+}
+
+if (n > 98)
+{
+for (count = n; count >= 98; count--)
+{
+printf("%d", count);
+if (count > 97)
+{
+printf(", ");
+}
+}
+}
+printf("\n");
+}
