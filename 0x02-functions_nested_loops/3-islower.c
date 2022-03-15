0c83649e21117d63ec8fe620c534cb98e1a87d83
bishop254 <karanjachiuri@gmail.com>
2022-03-15 22:10:43 +0300

    Updated the fourth script

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - main function
- * Description: checks if a character is lowercase 
+ * Description: checks if a character is lowercase
  * @c: letter of type char
  * Return: 1 if c is lowercase, otherwise 0
  */
