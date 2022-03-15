266763292168152ea2861306b2210241ae7343a4
bishop254 <karanjachiuri@gmail.com>
2022-03-16 00:25:05 +0300

    Added the eleventh c file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - main function
+ * Description: prints the summation of two numbers
+ * @n: first integer for addition
+ * @m: second integer for addition
+ * Return: result of addition
+ */
+
+int add(int n, int m)
+{
+int sum;
+sum = n + m;
+return (sum);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,36 +8,36 @@
 
 void times_table(void)
 {
-int factor;
-int count, result;
+    int factor;
+    int count, result;
 
-for (factor = 0; factor < 10; factor++)
-{
-for (count = 0; count < 10; count++)
-{
-result = factor * count;
+    for (factor = 0; factor < 10; factor++)
+    {
+        for (count = 0; count < 10; count++)
+        {
+            result = factor * count;
 
-if (result > 9)
-{
-_putchar((result / 10) + '0');
-_putchar((result % 10) + '0');
-}
-else if (count != 0)
-{
-_putchar(' ');
-_putchar(result + '0');
-}
-else
-{
-_putchar(result + '0');
-}
+            if (result > 9)
+            {
+                _putchar((result / 10) + '0');
+                _putchar((result % 10) + '0');
+            }
+            else if (count != 0)
+            {
+                _putchar(' ');
+                _putchar(result + '0');
+            }
+            else
+            {
+                _putchar(result + '0');
+            }
 
-if (count != 9)
-{
-_putchar(44);
-_putchar(' ');
-}
-}
-_putchar('\n');
-}
+            if (count != 9)
+            {
+                _putchar(44);
+                _putchar(' ');
+            }
+        }
+        _putchar('\n');
+    }
 }
