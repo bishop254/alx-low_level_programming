ef98cd543cb7cdb165e77e1e21e2405384837566
bishop254 <karanjachiuri@gmail.com>
2022-03-15 17:39:28 +0300

    Added the first script

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - main function 
+ * Description: prints _putchar using a custom header
+ * The main.h file has a prototype function called _putchar
+ * that is linked to a file called _putchar.c
+ * Return: 0 
+ */
+int main()
+{
+char mess[] = "_putchar";
+int position = 0;
+
+while(mess[position] != "\0")
+{
+_putchar(mess[position]);
+position++;
+}
+_putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+Nested loops in C
+
+0-putchar.c -> it has the main.h header file that calls the _putchar function defined in the _putchar.c file
