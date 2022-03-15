7773dfcf1bc1f450b8c76a4b57488fc1acfb5df1
bishop254 <karanjachiuri@gmail.com>
2022-03-15 21:53:35 +0300

    Updated the third script

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_alphabet - main function for printing alphabets.
- * Description: prints the alphabet using a custom header file function.
- * Return: 0
+ * print_alphabet_x10 - main function for printing alphabets
+ * Description: prints the alphabet ten times using a custom header file function.
+ * Return: void
  */
 void print_alphabet_x10(void)
 {
