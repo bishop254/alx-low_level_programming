121e7102f1dc9735d42afe19c5bc259b19fb69f3
bishop254 <karanjachiuri@gmail.com>
2022-03-15 22:35:40 +0300

    Added the seventh script

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _abs - main function
+ * @int: integer from which we will compute the absolute value
+ * Description: prints absolute value of an integer
+ * Return: absolute value
+ */
+
+int _abs(int n)
+{
+if (n > 0)
+{
+return(n);
+}
+else if (n == 0)
+{
+return (0);
+}
+else
+{
+n = n * (-1);
+return (n);
+}
+}
