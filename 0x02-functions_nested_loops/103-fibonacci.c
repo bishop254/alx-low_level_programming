d15f559427feb565defd687aaf307f1bbafdb826
bishop254 <karanjachiuri@gmail.com>
2022-03-16 15:02:02 +0300

    Added the fourth advanced c file

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Description: prints the first 50 even Fibonacci numbers 
+ * whose value is less than 4000000
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+long int f1 = 1, f2 = 2;
+long int nextNum = f1 + f2;
+long int sum = 0;
+
+for (i = 0; i <= 50; ++i)
+{
+f1 = f2;
+f2 = nextNum;
+nextNum = f1 + f2;
+if (nextNum < 4000000)
+{
+if (nextNum % 2 == 0)
+{
+sum += nextNum;
+}
+}
+else
+{
+break;
+}
+}
+printf("%lu", sum);
+printf("\n");
+return (0);
+}
