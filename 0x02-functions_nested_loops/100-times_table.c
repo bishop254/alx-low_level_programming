a25ea557a69dad029d511c96e4934fc4d3970305
bishop254 <karanjachiuri@gmail.com>
2022-03-16 10:48:25 +0300

    Updated the first advanced c file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,16 +10,13 @@
 
 void print_times_table(int n)
 {
-int factor;
-int count, result;
-if (n > 0 || n < 16)
-{
+int factor, count, result;
+if (n < 0 || n > 15) return;
 for (factor = 0; factor <= n; factor++)
 {
 for (count = 0; count <= n; count++)
 {
 result = factor * count;
-
 if (result > 99)
 {
 _putchar((result / 100) + '0');
@@ -42,7 +39,6 @@ else
 {
 _putchar(result + '0');
 }
-
 if (count != n)
 {
 _putchar(44);
@@ -50,6 +46,5 @@ _putchar(' ');
 }
 }
 _putchar('\n');
-}
 }
 }
