6fd2f5668d4bd10fb4e8067d7acedf784f98a211
bishop254 <karanjachiuri@gmail.com>
2022-03-16 12:54:43 +0300

    Updated the third advanced c file

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,11 +11,11 @@ int i;
 long int f1 = 1, f2 = 2;
 long int nextNum = f1 + f2;
 
-printf("%d, %d, ", f1, f2);
+printf("%lu, %lu, ", f1, f2);
 
 for (i = 3; i <= 50; ++i)
 {
-printf("%d, ",nextNum);
+printf("%lu, ",nextNum);
 f1 = f2;
 f2 = nextNum;
 nextNum = f1 + f2;
