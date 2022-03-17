dba2b956571aa8e988bbf03fa147e7bdc73b6ea6
bishop254 <karanjachiuri@gmail.com>
2022-03-17 19:31:51 +0300

    Added the seventh c file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - main function
+ * @n: number of underscores
+ * Description: prints a line on the terminal
+ * Return: void
+ */
+
+void print_line(int n)
+{
+int count = 0;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (count = 0; count <= n; count++)
+{
+_putchar('95');
+}
+_putchar('\n');
+}
+}
