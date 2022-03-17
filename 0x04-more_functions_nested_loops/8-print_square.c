09faedbf7b1b94ca153fd3b76481143f3b1ebf0f
bishop254 <karanjachiuri@gmail.com>
2022-03-17 21:14:13 +0300

    Added the ninth c file

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - main function
+ * @size: measure of the squares' side
+ * Description: prints a square on the terminal
+ * Return: void
+ */
+
+void print_square(int size)
+{
+int s1, s2;
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for(s2 = 0; s2 < size; s2++)
+{
+for (s1 = 0; s1 < size; s1++)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
+}
+}
