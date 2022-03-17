b60e0f5eff3cbdd7bdcad5df3da75495dbc95da1
bishop254 <karanjachiuri@gmail.com>
2022-03-17 21:53:32 +0300

    Added the last c file

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - main function
+ * @size: measure of the triangles' side
+ * Description: prints a triangle on the terminal using "#"
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+int count, b = size - 1, h;
+if (size <= 0)
+{
+_putchar('\n');
+}
+
+for (count = 0; count < size; count++)
+{
+b = size - count - 1;
+h = count + 1;
+for (b; b > 0; b--)
+{
+_putchar(' ');
+}
+for (h; h > 0; h--)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
+}
