fd0f74312b8b95146efdd194eae6ad2445df0af5
bishop254 <karanjachiuri@gmail.com>
2022-03-21 11:05:27 +0300

    Added a c file that swaps the values of two variables using pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int: main function
+ * Description: interchanges values stored at pointer variables a and b
+ * @a: first pointer variable to be swapped
+ * @b: second pointer variable to be swapped
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+int temp;
+temp = *a;
+*a = *b;
+*b = temp;
+return;
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,3 +1,5 @@
 All about pointers
 
 0-reset_to_98.c -> Takes a pointer as a function argument and changes the value of the variable address it points to to be 98.
+
+void swap_int(int *a, int *b) -> swaps the values of the two arguments
