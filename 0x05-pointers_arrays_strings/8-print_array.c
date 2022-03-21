39662a6c5ae85a0fb778ae73908a9954ef25aff0
bishop254 <karanjachiuri@gmail.com>
2022-03-21 13:10:32 +0300

    Updated the C file which prints n elements in an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,12 +12,12 @@ void print_array(int *a, int n)
 int i;
 for (i = 0; i < n; i++)
 {
-_putchar(a[i]);
+printf("%d", a[i]);
 while (i < n)
 {
-_putchar(',');
-_putchar(' ');
+printf(", ");
 }
 }
+printf('\n');
 return;
 }
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,4 +1,4 @@
-#All about pointers
+# All about pointers
 
 ## 0-reset_to_98.c
 Takes a pointer as a function argument and changes the value of the variable address it points to to be 98.
