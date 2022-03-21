4aa93554ba096c97f7992d873c24793bdf619389
bishop254 <karanjachiuri@gmail.com>
2022-03-21 10:54:59 +0300

    Updated the description to 0-...

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,11 +2,10 @@
 
 /**
  * reset_to_98: main function
- * @n: pointer to an interger variable
  * Description: Updates the value the pointer points to to be 98
+ * @n: pointer to an interger variable
  * Return: void
  */
-
 void reset_to_98(int *n)
 {
 *n = 98;
