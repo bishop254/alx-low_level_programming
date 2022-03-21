4c8dcaeb22ecb8a77beabfb81f5b8b7d89aa7ece
bishop254 <karanjachiuri@gmail.com>
2022-03-21 13:52:08 +0300

    Updated the C file that swaps strings using pointers stored in different variables

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,9 +2,9 @@
 
 /**
  * *_strcpy: main function.
- * Description: Copies a string from one pointer to another
  * @dest: pointer to the destination address
  * @src: pointer to the string that is being copied
+ * Description: Copies a string from one pointer to another
  * Return: void
  */
 char *_strcpy(char *dest, char *src)
