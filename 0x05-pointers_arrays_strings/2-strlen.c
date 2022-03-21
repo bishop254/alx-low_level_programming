1bb692031dcd01ef8f6f9345d4cdc8cad2f2df51
bishop254 <karanjachiuri@gmail.com>
2022-03-21 11:14:39 +0300

    Updated the description of 2-...

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * _strlen: main function
- * Description: Returns the length of a string
+ * _strlen: Returns the length of a string 
  * @s: A poointer pointing to a variable that stores a string
  * Return: length of the string
  */
