c1a466120d73c53eab8ebff51e27a2f8688cb576
bishop254 <karanjachiuri@gmail.com>
2022-03-26 01:06:19 +0300

    Updated various C files with varying functionalities

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,13 +6,14 @@
  * Description: Capitalize screen based on certain parameters
  * Return: Capitalized characters in a string.
  */
-char *cap_string(char *c){
+char *cap_string(char *c)
+{
 int i = 0, j;
 
 int punc[] = {32, '\t', 125, 123, 41, 40, 34, 33, 63, 46, 59, 44, 11, '\n'};
 if (c[0] > 96 && c[0] < 123)
 {
-c[0] -=32;
+c[0] -= 32;
 }
 
 while (c[i] != '\0')
@@ -22,7 +23,7 @@ if (c[i] > 96 && c[i] < 123)
 j = 0;
 while (j < 14)
 {
-if (c[i-1] == punc[j])
+if (c[i - 1] == punc[j])
 {
 c[i] -= 32;
 break;
@@ -31,6 +32,6 @@ j++;
 }
 i++;
 }
-return (c);
 }
+return (c);
 }
